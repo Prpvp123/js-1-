7c5d3055c26bd4b3d483a4d2b495a7af7a87d0b7
Prpvp123 <144214380+Prpvp123@users.noreply.github.com>
2024-01-23 11:43:47 +0300

    Add files via upload

---
+++ js 1 урок/main.js
@@ -0,0 +1,147 @@
+// Работа с переменными1
+//  let num = 3;
+//  alert(num);
+//  console.log;
+
+
+// Работа с переменными 2
+// let a = 10;
+// let b = 2;
+// let sum = a + b;
+// let difference = a - b;
+// let product = a * b;
+// let quotient = a / b;
+// alert("Сумма: " + sum);
+// alert("Разность: " + difference);
+// alert("Произведение: " + product);
+// alert("Частное: " + quotient);
+// console.log;
+
+// Работа с переменными 3
+// let c = 15;
+// let d = 2;
+// let result = c + d;
+// alert(result);
+// console.log
+
+// Работа с переменными 4
+// let a = 10;
+// let b = 2;
+// let c = 5;
+// alert = a + b + c
+// print(alert)
+
+// Работа с переменными 5
+// a = 17
+// b = 10
+// c = a - b
+// d = 7
+// result = c + d
+// print(result)
+
+//1задачи Работает! 
+    // name = prompt('Как вас зовут?');
+    // let str = ''; 
+    // let age = prompt('Сколько вам лет?');
+    // for (i = 1; i <= 3; i++) {
+    //     str += 'Happy birthday to you \n';
+    // }
+    // console.log(str); 
+
+    // 2 Задачи 
+// let a = 1, b = 1;
+//     let c = ++a; // ?
+//     let d = b++; // ?
+//     console.log(c , d);
+
+// 3 Задачи
+// let a = 2;
+// let x = 1 + (2 *= 2);
+// console.log(x)
+
+// Работа со строками 1
+// str = 'Привет, Мир!'
+
+// print(str)
+// Работа со строками 2
+// str1 = 'Привет, '
+// str2 = 'Мир!'
+
+// print(str1 + str2)
+// Работа со строками 3
+// name = 'Илья'
+
+// print('Привет, %s!' % name)
+// Работа со строками 4 
+// age = 25
+
+// print('Мне %d лет!' % age)
+
+// Функция prompt 1
+// name = prompt('Как вас зовут?')
+// alert('Ваше имя %s' % name)
+
+// Функция prompt 2
+// num = prompt('Введите число')
+// alert(num ** 2)
+
+// Обращение к символам строки 1
+// str = 'abcde'
+// print(str[0])
+// print(str[2])
+// print(str[4])
+
+// // Обращение к символам строки 2
+// num = '12345'
+// result = 1
+// for i in num:
+// result *= int(i)
+// print(result)
+
+
+// Практика 1
+// количество секунд в часе
+// hour_seconds = 60 * 60
+// print('Количество секунд в часе:', hour_seconds)
+
+// количество секунд в сутках
+// day_seconds = hour_seconds * 24
+// print('Количество секунд в сутках:', day_seconds)
+
+// количество секунд в месяце
+// month_seconds = day_seconds * 30
+// print('Количество секунд в месяце:', month_seconds)
+
+// 2Практика
+// hour = 12
+// minute = 34
+// second = 56
+
+// print('%02d:%02d:%02d' % (hour, minute, second))
+
+// 3Практика
+// num = 5
+// square = num ** 2
+// print(square)
+
+
+
+// Работа с присваиванием и декрементами
+
+// var num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num)
+
+
+// Работа с присваиванием и декрементами
+// var num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num)
+
+
+
